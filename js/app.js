edac7fead4628d7d3340a1339766bf7094ea7286
Madeline Watson <mcwatson91@gmail.com>
2018-09-27 15:35:38 -0400

    search bar functionality

--- js/app.js
+++ js/app.js
@@ -1,10 +1,8 @@
 // global variables for HTML elements
 const page = $("body");
 const $container = $(".container");
-// const $employeeBox = $(".employees");
-const userInput = document.getElementById("search");
+const $userInput = $("#search");
 const $searchButton = $(".button");
-// const $modalBox = $(".details");
 const $closeButton = $(".close");
 const $myModal = $(".modal");
 
@@ -22,7 +20,7 @@ class Employee {
   }
 
   // create new div for each employee
-  createDiv () {
+  createDiv() {
     let div = `
       <div class="box">
       <img src="${this.picture.medium}">
@@ -35,9 +33,7 @@ class Employee {
     return div;
   }
 
-}
-
-const createModalWindow = (profile) => {
+ createModalWindow(profile) {
 
   const birthdayFormat = (bday) => {
     // get date
@@ -47,20 +43,21 @@ const createModalWindow = (profile) => {
 
   let window = `
     <div class="modal">
-      <span class="close">&times;</span>
-        <img src="${profile.picture.large}">
-          <h3 class="name">${profile.name.first} ${profile.name.last}</h3>
-            <p>${profile.login.username}</p>
-            <p class="email">${profile.email}</p>
-            <p class="location">${profile.location.city}</p><br>
-            <p>${profile.cell}</p>
-            <p class="location">${profile.location.street} ${profile.location.city}, ${profile.location.state} ${profile.location.postcode}</p>
-            <p class="birthday">Birthday: ${birthdayFormat(profile.dob.date)}</p>
+        <img src="${this.picture.large}">
+        <span class="close">X</span>
+          <h3>${this.name.first} ${this.name.last}</h3>
+            <p class="username">${this.login.username}</p>
+            <p class="email">${this.email}</p>
+            <p class="location">${this.location.city}</p><br>
+            <p>${this.cell}</p>
+            <p class="location">${this.location.street} ${this.location.city}, ${this.location.state} ${this.location.postcode}</p>
+            <p class="birthday">Birthday: ${birthdayFormat(this.dob.date)}</p>
             <a href="#" class="prev">&lArr;</a>
             <a href="#" class="next">&rArr;</a>
     </div>
   `;
   return window;
+  }
 }
 
 // array to hold data received using Fetch API
@@ -69,7 +66,7 @@ let employeeArray = [];
 // use Fetch to retrieve data for 12 random employees and parse to JSON
 const getRandomEmployee = () => {
   return fetch('https://randomuser.me/api/?results=12&inc=name,location,email,picture,login,cell,dob&nat=NZ,US')
-  .then(res => res.json())
+  .then(response => response.json())
   .then(data => {return data.results});
 }
 
@@ -84,27 +81,20 @@ function appendEmployee() {
       });
       employeeArray.map(employee => {
         const square = employee.createDiv();
-        const modalWindow = createModalWindow(employee);
-
         $container.append(square);
+
+        const modalWindow = employee.createModalWindow();
         $container.append(modalWindow);
 
         $(".modal").hide();
-      })
-      //loop through the array that was just created to show various values for the different object keys
-    //   function captureCity() {
-    //   employeeArray.forEach((element, index, array) => {
-    //       console.log(element.location.city);
-    //       // console.log(index);
-    //       // console.log(array);
-    //   });
-    // }
+      });
 
-    function toggleModalWindow() {
-      // write code to switch between "normal" viewing page and modal page,
-      // then call this function using conditional statements in the click functions
-      // below?
-    }
+
+    // function toggleModalWindow() {
+    //   write code to switch between "normal" viewing page and modal page,
+    //   then call this function using conditional statements in the click functions
+    //   below?
+    // }
 
 
     $(".box").on("click", function() {
@@ -127,20 +117,42 @@ function appendEmployee() {
 
     $(".next").on("click", function() {
       let parent = $(this).parent(); // gets the modal window div currently displaying
-      let lastParent = employeeArray[employeeArray.length - 1]; // gets the last modal window div in the list
-      if ( lastParent ) {
-        console.log("Null!");
-        $(this).css('pointer-events', 'none'); // doesn't work to disable the mouse click - find something else
-      } else {
-        console.log("Didn't work...");
+      // let lastParent = employeeArray[employeeArray.length - 1]; // gets the last modal window div in the list?
+      // if ( lastParent ) {
+      //   console.log("Null!");
+      //   $(this).css('pointer-events', 'none'); // doesn't work to disable the mouse click - find something else
+      // } else {
+      //   console.log("Didn't work...");
         parent.hide();
         parent.next().next().show();
+      })
+    // });
+
+    // search bar function below
+
+    $userInput.on('keyup', function(){
+      let userSearch = $userInput.val().toLowerCase();
+      let names = $('.name');
+      let username = $('.username');
+
+      for (i = 0; i < names.length; i += 1) {
+        if ( names[i].innerHTML.indexOf(userSearch) > -1 ) {
+          names[i].parentElement.parentElement.style.display = "";
+        } else {
+          names[i].parentElement.parentElement.style.display = 'none';
+        }
       }
+
+      for (i = 0; i < username.length; i += 1) {
+        if ( username[i].innerHTML.indexOf(userSearch) > -1 ) {
+          names[i].parentElement.parentElement.style.display = "";
+        } else {
+          names[i].parentElement.parentElement.style.display = 'none';
+        }
+      }
+
     });
 
-    // search bar function below:
-
-    
 
     });
 
