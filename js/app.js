c82a7c9a721671a7bb54162e0fc90b90f766415f
Madeline Watson <mcwatson91@gmail.com>
2018-09-28 15:06:03 -0400

    css styles

--- js/app.js
+++ js/app.js
@@ -2,19 +2,17 @@
 const page = $("body");
 const $container = $(".container");
 const $userInput = $("#search");
-const $searchButton = $(".button");
 const $closeButton = $(".close");
 const $myModal = $(".modal");
 
 // class constructor to create random employee
 class Employee {
 
-  constructor (name, location, email, picture, login, cell, dob) {
+  constructor (name, location, email, picture, cell, dob) {
     this.name = name;
     this.location = location;
     this.email = email;
     this.picture = picture;
-    this.login = login;
     this.cell = cell;
     this.dob = dob;
   }
@@ -33,6 +31,7 @@ class Employee {
     return div;
   }
 
+//create modal window for each employee
  createModalWindow(profile) {
 
   const birthdayFormat = (bday) => {
@@ -43,13 +42,14 @@ class Employee {
 
   let window = `
     <div class="modal">
+    <span class="close">X</span>
+      <div class="pic_wrap">
         <img class="modal_img" src="${this.picture.large}">
-        <span class="close">X</span>
+      </div>
         <div class="modal_details">
             <h3 class= "modal_name">${this.name.first} ${this.name.last}</h3>
-            <p class="modal_username">${this.login.username}</p>
             <p class="modal_email">${this.email}</p>
-            <p class="modal_location">${this.location.city}</p><br>
+            <p class="modal_location">${this.location.city}</p>
             <hr>
             <p>${this.cell}</p>
             <p class="modal_location">${this.location.street} ${this.location.city}, ${this.location.state} ${this.location.postcode}</p>
@@ -65,23 +65,23 @@ class Employee {
   }
 }
 
-// array to hold data received using Fetch API
+// empty array to hold data received using Fetch API
 let employeeArray = [];
 
 // use Fetch to retrieve data for 12 random employees and parse to JSON
 const getRandomEmployee = () => {
-  return fetch('https://randomuser.me/api/?results=12&inc=name,location,email,picture,login,cell,dob&nat=NZ,US')
+  return fetch('https://randomuser.me/api/?results=12&inc=name,location,email,picture,cell,dob&nat=NZ,US')
   .then(response => response.json())
   .then(data => {return data.results});
 }
 
-// take the retrieved data and append one employee per box
+// take the retrieved data and append one employee per box div
 function appendEmployee() {
   getRandomEmployee()
     .then(results => {
       const employees = results;
       employees.map(employee => {
-        const empl = new Employee(employee.name, employee.location, employee.email, employee.picture, employee.login, employee.cell, employee.dob);
+        const empl = new Employee(employee.name, employee.location, employee.email, employee.picture, employee.cell, employee.dob);
         employeeArray.push(empl);
       });
       employeeArray.map(employee => {
@@ -94,55 +94,48 @@ function appendEmployee() {
         $(".modal").hide();
       });
 
-
+  //click function to bring up modal window
     $(".box").on("click", function() {
       $(this).next().show();
       $("#search, .button, .box").addClass("disabled");
       $("body").addClass("overlay");
     });
 
+  // click function to close modal window
     $(".close").on("click", function() {
       $(this).parent().hide();
       $("#search, .button, .box").removeClass("disabled");
       $("body").removeClass("overlay");
     });
 
-
+  // disable the "next" button on the last modal window
     $(".arrow_buttons:eq(11) > .next").addClass("disabled");
 
+  // click function for each next button on the modal window
     $(".next").on("click", function() {
       let parent = $(this).parent().parent(); // gets the modal window div currently displaying
         parent.hide();
         parent.next().next().show();
     });
 
+  // disable the "prev" button on the first modal window
     $(".arrow_buttons:eq(0) > .prev").addClass("disabled");
 
+  // click function for each previous button on the modal window
     $(".prev").on("click", function() {
       let parent = $(this).parent().parent();
         parent.hide();
         parent.prev().prev().show();
     });
 
-
-    // search bar function that responds to usernames or names
-
+    // search bar function that filters employees by name
     $userInput.on('keyup', function(){
       let userSearch = $userInput.val().toLowerCase();
       let names = $('.name');
-      let username = $('.modal_username');
 
       for (i = 0; i < names.length; i += 1) {
         if ( names[i].innerHTML.indexOf(userSearch) > -1 ) {
-          names[i].parentElement.parentElement.style.display = "";
-        } else {
-          names[i].parentElement.parentElement.style.display = 'none';
-        }
-      }
-
-      for (i = 0; i < username.length; i += 1) {
-        if ( username[i].innerHTML.indexOf(userSearch) > -1 ) {
-          names[i].parentElement.parentElement.style.display = "";
+          names[i].parentElement.parentElement.style.display = '';
         } else {
           names[i].parentElement.parentElement.style.display = 'none';
         }
